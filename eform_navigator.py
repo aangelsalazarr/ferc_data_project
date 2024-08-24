6459d5324ad161d469f086e8a5a782e436fef35d
Angel Salazar <aangelsalazarr@gmail.com>
2024-08-24 14:54:41 -0500

    working on form type

--- eform_navigator.py
+++ eform_navigator.py
@@ -32,29 +32,58 @@ driver = webdriver.Chrome()
 # opening the ferc eform page
 driver.get(url=eform_url)
 
-# this will be the xpath we want to check for to make sure the webpage has loaded....
-eform_tbl = '//*[@id="myGrid"]'
+# xpath locating the upper left cell within the eform table
+eform_tbl_row1 = '/html/body/div/app-root/submission-history/div/ag-grid-angular/div/div[1]/div/div[3]/div[2]/div/div/div[1]/div[1]'
 
 # waiting for webpage to fully load and we know this by the existence of the table with form data
-WebDriverWait(driver, 15).until(lambda x: x.find_element(By.XPATH, eform_tbl)).is_displayed()
+WebDriverWait(driver, 17).until(EC.presence_of_all_elements_located((By.XPATH, eform_tbl_row1)))
 
-'''# allowing the website to load for 7 seconds
-driver.implicitly_wait(time_to_wait=7)
+# letting the user know that the eform table has been identified
+print('Table containing eforms is present (=')
+
+# now we want to filter out by year
+year_button_path = '/html/body/div/app-root/submission-history/div/ag-grid-angular/div/div[1]/div/div[1]/div[2]/div/div/div[4]/div[3]/span'
+year_button = driver.find_element(By.XPATH, year_button_path)
+year_button.click()
+
+# now we want to input string value represetning year we would like to filter for year, e.g. 2024
+year_filter_input = '/html/body/div/app-root/submission-history/div/ag-grid-angular/div/div[3]/div/div/div/div[1]/div[1]/div[1]/input'
+year_filter = driver.find_element(By.XPATH, year_filter_input)
+year_filter.send_keys('2024')
+
+# purpose is to click apply filter on the year filter
+apply_year_filter_path = '/html/body/div[1]/app-root/submission-history/div/ag-grid-angular/div/div[3]/div/div/div/div[2]/button[2]'
+apply_year_filter = driver.find_element(By.XPATH, apply_year_filter_path)
+apply_year_filter.click()
+
+# purpose is to let used select the type of form now
+'''
+Possible string inputs: 
+Form 1, Form 1F, Form 2, Form 2A, Form 3Q Electric, Form 3Q Gas, Form 6, Form 60, Form 6Q, Form 714
+'''
+
+# list of potential forms we can filter for
+form_types = ['Form 1', 'Form 1F', 'Form 2', 'Form 2A', 'Form 3Q Elecrtric', 'Form 3Q Gas', 
+              'Form 6', 'Form 60', 'Form 6Q', 'Form 714']
+
+# finding form filter drop down menu
+form_button_path = '/html/body/div[1]/app-root/submission-history/div/ag-grid-angular/div/div[1]/div/div[1]/div[2]/div/div/div[3]/div[3]/span'
+form_button = driver.find_element(By.XPATH, form_button_path)
+form_button.click()
+
+# now we are inserting the form type we want to filter for
+form_filter_input = ''
+form_filter = driver.find_element(By.XPATH, form_filter_input)
+form_filter.send_keys('Form 714')
+
+# purpose is to click on the search icon to apply filter
+apply_form_filter_path = '/html/body/div[1]/app-root/submission-history/div/ag-grid-angular/div/div[3]/div/div/set-filter/div/div[3]/div/mat-form-field/div/div[1]/div[2]'
+apply_form_filter = driver.find_element(By.XPATH, apply_form_filter_path)
+apply_form_filter.click()
 
 # letting the webpage chill out for a bit to allow for the eforms site to load
-# webpage loads in a little under 10 seconds so setting at 10 should be sufficient
-time.sleep(10)'''
-
-# setting up the clicking of the Year column carrot via xpath
-year_col_button_path = '//*[@id="caret-down"]'
-
-# this is where we input the year string YYYY
-year_input_str = '/html/body/div/app-root/submission-history/div/ag-grid-angular/div/div[3]/div/div/div/div[1]/div[1]/div[1]'
-
-# this code clicks on the carrot drop down for year col specifically
-year_col_filter = driver.find_element(By.XPATH, year_col_button_path)
-year_col_filter.click()
-
+# webpage loads in a little under 7 seconds so setting at 10 should be sufficient
+time.sleep(15)
 
 # closing the window of our loaded webpage
 driver.quit()
